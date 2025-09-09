d3392880bf737a2d3eadab6aecd0a2c3b9b7ac75
Kawan Silva <kwnsilva@hotmail.com>
2025-09-09 12:22:58 -0300

    feat: Update Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,7 @@ const Header = () => {
               <Code2 className="w-6 h-6 text-white" />
             </div>
             <span className="text-xl font-bold bg-gradient-primary bg-clip-text text-transparent">
-              DevPortfólio
+              Kawan Silva
             </span>
           </Link>
 
