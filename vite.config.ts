8f8936de2617b0e2938e6a74d589ebd7ff47e844
Kawan Silva <kwnsilva@hotmail.com>
2025-09-09 09:07:59 -0300

    feat: Configure GitHub Pages deploy with GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// Altere para o nome do seu repositório no GitHub
+const repoName = "/vite_react_shadcn_ts/";
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 3000,
   },
+  base: repoName,
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
